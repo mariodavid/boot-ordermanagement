a65aabe33c3e353e1ac09885f1c1282761272d82
Mario David <mariodavid@arcor.de>
2017-09-04 09:42:45 +0200

    initial commit

---
+++ src/main/java/de/diedavids/bo/BootOrdermanagementApplication.java
@@ -0,0 +1,12 @@
+package de.diedavids.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOrdermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOrdermanagementApplication.class, args);
+	}
+}
---
+++ src/main/java/de/diedavids/bo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package de.diedavids.bo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "boot-ordermanagement";
+    }
+}
